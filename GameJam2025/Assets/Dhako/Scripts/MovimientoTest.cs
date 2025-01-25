441965a35606239cd35b146570121c73d0af54d8
Daniel j <daniandres198@gmail.com>
2025-01-24 22:21:30 -0500

    test primera escena

---
+++ GameJam2025/Assets/Dhako/Scripts/MovimientoTest.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoTest : MonoBehaviour
+{
+    public float speed = 5f; 
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+       
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+        movement.Normalize();
+        //transform.position += movement * speed * Time.deltaTime;
+        rb.AddForce(movement * speed);
+    }
+    
+}
