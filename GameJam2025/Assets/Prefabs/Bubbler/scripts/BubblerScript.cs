8e09b425c7e2225c7b286b82308dbe80c91e15cb
Zeit <shecksk8nick@gmail.com>
2025-01-24 22:57:10 -0500

    Created main prefab

---
+++ GameJam2025/Assets/Prefabs/Bubbler/scripts/BubblerScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubblerScript : MonoBehaviour
+{
+    [SerializeField] public float sensivity = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Inspect()
+    {
+
+    }
+
+    void Walk()
+    {
+
+    }
+
+    void Sneak()
+    {
+
+    }
+}
