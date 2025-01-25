f80e8b716871239624c568d63e4cbfc1a7f7715b
Zeit <shecksk8nick@gmail.com>
2025-01-25 00:32:15 -0500

    Created bubbler

--- GameJam2025/Assets/Prefabs/Bubbler/scripts/BubblerScript.cs
+++ GameJam2025/Assets/Prefabs/Bubbler/scripts/BubblerScript.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class BubblerScript : MonoBehaviour
 {
-    [SerializeField] public float sensivity = 10.0f;
+    [SerializeField] public float speed = 10f;
+    private bool crouching = false;
+
+
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        Walk();
+        Crouch();
     }
 
     void Inspect()
@@ -24,11 +27,18 @@ public class BubblerScript : MonoBehaviour
 
     void Walk()
     {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
 
+        if (horizontalMovement != 0 || verticalMovement != 0)
+        {
+            Vector3 movement = new Vector3(horizontalMovement, 0, verticalMovement) * speed * Time.deltaTime;
+            transform.Translate(movement, Space.Self);
+        }
     }
 
-    void Sneak()
+    void Crouch()
     {
-
+        crouching = Input.GetButton("Crouch");
     }
 }
