db223e18781247f5b3648db7805fef35c1796207
Daniel j <daniandres198@gmail.com>
2025-01-25 20:07:45 -0500

    mesas

--- GameJam2025/Assets/Prefabs/InteractableObject/Scripts/MesaCollectible.cs
+++ GameJam2025/Assets/Prefabs/InteractableObject/Scripts/MesaCollectible.cs
@@ -2,8 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MesaCollectible : MonoBehaviour
+public class MesaCollectible : MonoBehaviour, IInteractable
 {
+
+    private int count = 1;
+    private float cooldown= 5;
+    private bool startCooldown = false;
+
+    [SerializeField] private GameObject PicableObjPrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,35 @@ public class MesaCollectible : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (startCooldown) cooldown -= 1 * Time.deltaTime;
+        if (cooldown < 0) startCooldown = false;
+    }
+
+    public string GetAction()
+    {
+        if (count >= 1)
+        {
+            string action = "Take";
+            return action;
+        }
+        if (cooldown <= 0)
+        {
+            string action2 = "Inspect";
+            return action2;
+        }
+        return null;
+       
+    }
+
+    public GameObject GetObject()
+    {
+        if (count >=1)
+        {
+            count--;
+            startCooldown = true;
+            return PicableObjPrefab;
+        }
+        else return null;
+
     }
 }
