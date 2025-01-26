eef61d148a86be4b446ad9c530dfa0732d28a015
Johan Miranda <johanops@MacBook-Pro.local>
2025-01-26 11:39:55 -0500

    Menu Perder

---
+++ GameJam2025/Assets/Syntax/Scripts/MenuPerdiste.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+
+public class MenuPerdiste : MonoBehaviour
+{
+    [FormerlySerializedAs("perdisteMenuUI")] public GameObject perdisteMenuUI;
+
+    void Update()
+    {
+        
+    }
+    
+    public void ShowMenu()
+    {
+        perdisteMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+        perdisteMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        
+    }
+
+    void Pause()
+    {
+        perdisteMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Renaudar()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu"); //Se pone primer escena del juego
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Saliendo del juego...");
+        Application.Quit();
+    }
+}
