f3fb7cab6f155de0cc82ed7bb063831700fada9d
Johan Miranda <johanops@MacBook-Pro.local>
2025-02-08 14:12:18 -0500

    Se hacen los botones interactuables

--- GameJam2025/Assets/Syntax/Scripts/MenuInicio.cs
+++ GameJam2025/Assets/Syntax/Scripts/MenuInicio.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class ShowImageWithSmoothTransition : MonoBehaviour
 {
@@ -10,10 +12,18 @@ public class ShowImageWithSmoothTransition : MonoBehaviour
     private void Start()
     {
         if (imageCanvasGroup != null)
+        {
             imageCanvasGroup.alpha = 1;
+            imageCanvasGroup.interactable = true;
+            imageCanvasGroup.blocksRaycasts = true;
+        }
 
         if (menuCanvasGroup != null)
+        {
             menuCanvasGroup.alpha = 0;
+            menuCanvasGroup.interactable = false;
+            menuCanvasGroup.blocksRaycasts = false;
+        }
 
         StartCoroutine(ShowImageThenMenuWithSmoothTransition());
     }
@@ -39,9 +49,29 @@ public class ShowImageWithSmoothTransition : MonoBehaviour
         }
 
         if (imageCanvasGroup != null)
+        {
             imageCanvasGroup.alpha = 0;
+            imageCanvasGroup.interactable = false;
+            imageCanvasGroup.blocksRaycasts = false;
+        }
+
 
         if (menuCanvasGroup != null)
+        {
             menuCanvasGroup.alpha = 1;
+            menuCanvasGroup.interactable = true;
+            menuCanvasGroup.blocksRaycasts = true;
+        }
+    }
+    public void StartGame()
+    {
+        Debug.Log("Iniciar juego...");
+        SceneManager.LoadScene("FinalFinal");
+    }
+    
+    public void ExitGame()
+    {
+        Debug.Log("Salir del juego...");
+        Application.Quit();
     }
 }
