7bd4cb046647eb866ca2ee1f709daaf8775c4923
Daniel j <daniandres198@gmail.com>
2025-02-12 23:05:31 -0500

    Tutorial

---
+++ GameJam2025/Assets/Dhako/Scripts/TutorialController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class TutorialController : MonoBehaviour
+{
+
+    [SerializeField] private RectTransform TutorialUI;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        TutorialUI.DOAnchorPos(new Vector2(0, -1953), 1f, true).SetEase(Ease.OutBack)
+            .OnComplete(PauseTime);
+
+
+    }
+
+    private void PauseTime()
+    {
+        Time.timeScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Interact"))
+        {
+            Time.timeScale = 1;
+            TutorialUI.DOAnchorPos(new Vector2(0, 0), 0.5f, true).SetEase(Ease.OutBack);
+
+        }
+    }
+}
